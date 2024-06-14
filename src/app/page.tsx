5d5f77799653d3e374ad091663a001d54d5642b1
Saurabh Dantani <django.developments@gmail.com>
2024-06-14 12:22:41 +0000

    improve code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-'use client'
+"use client";
 import HeaderIndex from "@/components/header/Index";
 import { GaugeCircleDemo } from "@/components/loader/IndexLoader";
 import Navbar from "@/components/navbar/Navbar";
 import { BoxRevealDemo } from "@/components/revealbox/Index";
+import { SkillIndex } from "@/components/skills";
 import { motion, AnimatePresence } from "framer-motion";
-import { useLayoutEffect, useRef, useState } from 'react';
+import { useLayoutEffect, useRef, useState } from "react";
 
 const pageTransition = {
   in: { opacity: 1, x: 0 },
@@ -38,34 +39,35 @@ export default function Home() {
         )}
         {showSecondComponent && (
           <main className="bg-gradient-to-r from-slate-500 to-stone-700 h-screen">
-        <Navbar />
-        <AnimatePresence mode="wait">
-          <motion.div
-            id="test1"
-            className="h-full"
-            initial="out"
-            animate="in"
-            exit="out"
-            variants={pageTransition}
-            transition={{ duration: 0.5 }}
-          >
-            {/* <HeaderIndex /> */}
-            <BoxRevealDemo />
-          </motion.div>
+            <Navbar />
+            <AnimatePresence mode="wait">
+              <motion.div
+                id="test1"
+                className="h-full"
+                initial="out"
+                animate="in"
+                exit="out"
+                variants={pageTransition}
+                transition={{ duration: 0.5 }}
+              >
+                {/* <HeaderIndex /> */}
+                <BoxRevealDemo />
+              </motion.div>
 
-          <motion.div
-            id="test2"
-            className="w-full h-[750%] bg-red-200"
-            initial="out"
-            animate="in"
-            exit="out"
-            variants={pageTransition}
-            transition={{ duration: 0.5 }}
-          >
-            <h1>hello</h1>
-          </motion.div>
-        </AnimatePresence>
-      </main>
+              <motion.div
+                id="test2"
+                className="w-full h-[750%] bg-red-200"
+                initial="out"
+                animate="in"
+                exit="out"
+                variants={pageTransition}
+                transition={{ duration: 0.5 }}
+              >
+                {/* <h1>hello</h1> */}
+                <SkillIndex />
+              </motion.div>
+            </AnimatePresence>
+          </main>
         )}
       </AnimatePresence>
       {/* <HeaderIndex /> */}
